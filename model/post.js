1ebc18869feb7ceeb0b47ddee99dc88bd5ebc2ba
Louay labidi <61356202+louayyy99@users.noreply.github.com>
2023-04-21 14:28:16 +0100

    Create post.js

---
+++ model/post.js
@@ -0,0 +1,32 @@
+import mongoose, {mongo} from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    date:{
+        type: Date,
+        required: true,
+    },
+    type:{
+        type: String,
+        required: true,
+    },
+    count:{
+        type: String,
+        required: true,
+    },
+    countType:{
+        type: String,
+        required: true,
+    },
+    text:{
+        type: String,
+        required: true,
+    },
+});
+
+const Post = mongoose.model('post', postSchema);
+
+export default Post;
